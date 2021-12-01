d05a8ffabaa0ed1ecadc615a155107be61969b9b
Jonas Delrue <jonas.delrue@jimber.io>
2021-12-01 21:19:42 +0100

    refactor folder structure

--- build-linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t rcplinux - < Dockerfile.linux
-docker rm -fv ygglinux || true
-docker run -d --name=ygglinux -v $(pwd)/src:/src rcplinux tail -f /dev/null
-docker exec ygglinux /bin/buildlinux #run this to build again without restarting docker
