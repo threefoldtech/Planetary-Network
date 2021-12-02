2aaf044f2d81f1b332289822ef1a2724f7d673fe
Mathias De Weerdt <mathias.de.weerdt@gmail.com>
2021-12-02 14:09:17 +0100

    Working MSI building

---
+++ scripts/windows-full-deploy/2.build-and-prepare-windows-binary-icon.bat
@@ -0,0 +1,3 @@
+@echo off
+echo "Inserting threefold ico in the executable binary"
+"C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe" -open "\\wsl.localhost\Ubuntu-18.04\home\singlecore\documents\jimber\TF-NetworkConnector\generated/builds/windows/ThreeFoldNetworkConnector.exe" -save "\\wsl.localhost\Ubuntu-18.04\home\singlecore\documents\jimber\TF-NetworkConnector\generated/builds/windows/ThreeFoldNetworkConnector.exe" -action addskip -res "\\wsl.localhost\Ubuntu-18.04\home\singlecore\documents\jimber\TF-NetworkConnector\generated/builds/windows/icon.ico" -mask ICONGROUP,MAIN
