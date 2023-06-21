bca5be6cf2fc40c48a5d77e249809d745da57531
Rob Van Mieghem <robvanmieghem@gmail.com>
2023-06-21 16:13:23 +0200

    Set the correct process and app name on osx

--- scripts/macos-full-deploy/1.build.sh
+++ scripts/macos-full-deploy/1.build.sh
@@ -13,11 +13,16 @@ qtdeploy build darwin ./
 
 cd ..
 
-echo "Renaming and adding reference to /Application directory path."
+echo overwriting the application bundle information
+cp "resources/Info.plist" "src/deploy/darwin/src.app/Contents/Info.plist"
+
+echo "Renaming"
 mv "src/deploy/darwin/src.app" "src/deploy/darwin/ThreeFold Planetary Network.app"
 
 echo "Adding icns to .app"
 cp "resources/src.icns" "src/deploy/darwin/ThreeFold Planetary Network.app/Contents/Resources/src.icns"
+
+echo "adding reference to /Application directory path."
 ln -s /Applications src/deploy/darwin/Applications
 
 echo "Generation executable dmg file."
